379467222c9892ae4afbb1d0b068ac4907d85221
Manoj-codes02 <2303031240444@paruluniversity.ac.in>
2026-05-11 09:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.84%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int square = (int) Math.sqrt(num);
+        if (square*square==num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
